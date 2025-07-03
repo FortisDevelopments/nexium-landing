fc15c4d0f9b0e80759301b84456377941e2850f7
Mike <miguelsz9711@gmail.com>
2025-07-03 14:09:09 -0600

    bg images

--- src/Pages/Booking/Booking.jsx
+++ src/Pages/Booking/Booking.jsx
@@ -1,22 +1,18 @@
 import React, { useEffect } from "react";
 import "./Booking.css";
 import Header from "../../Components/Header";
+
 const Booking = () => {
   useEffect(() => {
     // Load TidyCal embed script
-    const script = document.createElement("script");
-    script.src = "https://asset-tidycal.b-cdn.net/js/embed.js";
+    const script = document.createElement('script');
+    script.src = 'https://asset-tidycal.b-cdn.net/js/embed.js';
     script.async = true;
-    document.head.appendChild(script);
+    document.body.appendChild(script);
 
     // Cleanup function to remove script when component unmounts
     return () => {
-      const existingScript = document.querySelector(
-        'script[src="https://asset-tidycal.b-cdn.net/js/embed.js"]'
-      );
-      if (existingScript) {
-        document.head.removeChild(existingScript);
-      }
+      document.body.removeChild(script);
     };
   }, []);
 
@@ -25,10 +21,9 @@ const Booking = () => {
       <Header />
       <div className="booking-page">
         <h1 className="booking-title">Agenda una Cita con nosotros</h1>
-        <div
-          className="tidycal-embed"
-          data-path="megatron111994/15-minute-meeting"
-        ></div>
+        <div className="calendar-container">
+          <div className="tidycal-embed" data-path="megatron111994/15-minute-meeting"></div>
+        </div>
       </div>
     </>
   );
